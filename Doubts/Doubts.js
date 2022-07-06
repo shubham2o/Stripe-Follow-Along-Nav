2676c711693eb75eb5f847dbcbf59b4adeca4f0b
shubham2o <shubhamsharma.ha@gmail.com>
2022-07-06 21:34:03 +0530

    onmouseenter event and onmouseleave event added

---
+++ Doubts/Doubts.js
@@ -0,0 +1,40 @@
+// onmouseenter Event
+// EXAMPLE 2 || In HTML :
+function onmouseEnter() {
+    document.getElementById("demo").style.color = "red";
+}
+
+function onmouseLeave() {
+    document.getElementById("demo").style.color = "blue";
+}
+
+
+// EXAMPLE 2 || In JavaScript :
+document.getElementById("demo2").onmouseenter = function() {
+    onmouseEnter2();
+}
+
+function onmouseEnter2() {
+    document.getElementById("demo2").style.color = "orange";
+}
+
+document.getElementById("demo2").onmouseleave = function() {
+    onmouseLeave2();
+}
+
+function onmouseLeave2() {
+    document.getElementById("demo2").style.color = "purple";
+}
+
+
+// EXAMPLE 3 || In JavaScript, using the addEventListener() method :
+document.getElementById("demo3").addEventListener('mouseenter', mouseEnter);
+document.getElementById("demo3").addEventListener('mouseleave', mouseLeave);
+
+function mouseEnter() {
+    document.getElementById("demo3").style.color = "coral";
+}
+
+function mouseLeave() {
+    document.getElementById("demo3").style.color = "violet";
+}
